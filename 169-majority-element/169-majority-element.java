e89866451be580ad9a6bcc7b06aaf2b51bd83175
CharuSingh01 <77285892+CharuSingh01@users.noreply.github.com>
2022-08-02 00:50:41 +0530

    Time: 2 ms (89.29%), Space: 56.3 MB (39.90%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,3 +1,18 @@
+// Brute Force: 
+// for every value, traverse the whole array again and check if the frequency is > n/2
+// Time Complexity: O(N^2)
+
+// Better Approach:
+// Using HashMap
+// Time Complexity: between O(N) to O(NlogN)
+// Space Complexity: O(N)
+
+// Optimised Solution
+//Moore's Voting Solution
+// Cancel out minory elements with majority elements
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
 class Solution {
     public int majorityElement(int[] nums) {
         int count=1;

b75f551e3aa98feb046eafdff1ad76dd7ce41d9b
CharuSingh01 <77285892+CharuSingh01@users.noreply.github.com>
2022-08-10 22:46:12 +0530

    Time: 1 ms (80.19%), Space: 42.9 MB (67.40%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -8,36 +8,70 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// Brute Force Approach:
+// Use extra space by creating a new linked list.
+// Time Complexity: O(N)
+// Space Complexity: O(N)
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode prev=new ListNode(0,null);
+//         ListNode dummy=prev;
+//         while(list1!=null && list2!=null){
+//             if(list1.val<list2.val){
+//                 ListNode temp=new ListNode(list1.val,list1.next);
+//                 prev.next=temp;
+//                 prev=temp;
+//                 list1=list1.next;
+//             }
+//             else{
+//                ListNode temp=new ListNode(list2.val,list2.next);
+//                 prev.next=temp;
+//                 prev=temp;
+//                 list2=list2.next;
+//             }
+//         }
+//         while(list1!=null){
+//               ListNode temp=new ListNode(list1.val,list1.next);
+//                 prev.next=temp;
+//                 prev=temp;
+//                 list1=list1.next;
+//         }
+//         while(list2!=null){
+//             ListNode temp=new ListNode(list2.val,list2.next);
+//                 prev.next=temp;
+//                 prev=temp;
+//                 list2=list2.next;
+//         }
+//         return dummy.next;
+//     }
+// }
+
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode prev=new ListNode(0,null);
-        ListNode dummy=prev;
+        if(list1==null)return list2;
+        if(list2==null)return list1;
+       
+        if(list1.val>list2.val){
+            ListNode temp=list1;
+            list1=list2;
+            list2=temp;
+        }
+        ListNode dummy=list1;
         while(list1!=null && list2!=null){
-            if(list1.val<list2.val){
-                ListNode temp=new ListNode(list1.val,list1.next);
-                prev.next=temp;
-                prev=temp;
+            ListNode temp=null;
+            while(list1!=null && list1.val<=list2.val){
+                temp=list1;
                 list1=list1.next;
             }
-            else{
-               ListNode temp=new ListNode(list2.val,list2.next);
-                prev.next=temp;
-                prev=temp;
-                list2=list2.next;
-            }
+            temp.next=list2;
+             ListNode tempp=list1;
+            list1=list2;
+            list2=tempp;
         }
-        while(list1!=null){
-              ListNode temp=new ListNode(list1.val,list1.next);
-                prev.next=temp;
-                prev=temp;
-                list1=list1.next;
-        }
-        while(list2!=null){
-            ListNode temp=new ListNode(list2.val,list2.next);
-                prev.next=temp;
-                prev=temp;
-                list2=list2.next;
-        }
-        return dummy.next;
+        return dummy;
+        
     }
 }

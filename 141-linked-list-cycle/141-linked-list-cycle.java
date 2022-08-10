1ae88d05384d20cff3ac85bec9cbb667bd7d7c9c
CharuSingh01 <77285892+CharuSingh01@users.noreply.github.com>
2022-08-11 01:55:23 +0530

    Time: 0 ms (100.00%), Space: 46 MB (45.33%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -9,6 +9,17 @@
  *     }
  * }
  */
+// Brute Force:
+// Use hashmap to store nodes. if map.containsKey(node)==true , it will be a cycle.
+// Time Complexity: O(N) 
+// Space Complexity: O(N)
+
+// Optimised Approach:
+// Use fast and slow pointer approach
+//Time Complexity: O(N)
+// Space Complexity: O(1)
+
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow=head;

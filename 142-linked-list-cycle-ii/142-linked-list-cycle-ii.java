13c3c4b734a22891ddc0411bca4decc39f02a47f
CharuSingh01 <77285892+CharuSingh01@users.noreply.github.com>
2022-08-14 04:15:24 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (41.05%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -15,21 +15,50 @@
 // Time Complexity: O(N)
 // Space Complexity: O(N)
 
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
+//         ListNode answer=null;
+//      HashSet<ListNode> set=new HashSet<>();
+//         while(head!=null){
+//             if(set.contains(head)!=false){
+//                 answer=head;
+//                 break;
+//             }
+//             set.add(head);
+//             head=head.next;
+//         }
+//         return answer;
+         
+//     }
+// }
 
+// Optimised Approach:
+// use slow and fast pointer method
+// find the point of collision and now take two pointers. One at starting point and one at collision point. The place
+// where they collide is the answer
+// Time Complexity: O(N)
+// Space Complexity: O(1)
 
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        ListNode answer=null;
-     HashSet<ListNode> set=new HashSet<>();
-        while(head!=null){
-            if(set.contains(head)!=false){
-                answer=head;
-                break;
-            }
-            set.add(head);
-            head=head.next;
-        }
-        return answer;
-         
-    }
+      public ListNode detectCycle(ListNode head) {
+          if(head==null)return null;
+          ListNode answer=null;
+          ListNode slow=head;
+          ListNode fast=head;
+          while(fast.next!=null && fast.next.next!=null){
+              slow=slow.next;
+              fast=fast.next.next;;
+              if(fast==slow){
+                  answer=slow;
+                  break;
+              }
+          }
+          if(answer==null)return null;
+          while(head!=answer){
+              head=head.next;
+              answer=answer.next;
+          }
+          return head;
+          
+      }
 }

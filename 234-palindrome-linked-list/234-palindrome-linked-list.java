45924c674b5922848ca49d217fe71716652e39cd
CharuSingh01 <77285892+CharuSingh01@users.noreply.github.com>
2022-08-13 22:19:18 +0530

    Time: 9 ms (51.57%), Space: 104.1 MB (27.18%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -8,27 +8,72 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public boolean isPalindrome(ListNode head) {
-      if(head==null)return true;
-      temp=head;
-      boolean ans=solution(head);
-        return ans;
+
+// One approach is to use recursion
+
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//       if(head==null)return true;
+//       temp=head;
+//       boolean ans=solution(head);
+//         return ans;
         
+//     }
+//     ListNode temp;
+//     public boolean solution(ListNode right){
+//          if(right==null){
+//             return true;
+//         }
+//         boolean check=solution(right.next);
+//         if(check==false)return false;
+//         else if(temp.val!=right.val){
+//        return false;
+//         }
+//         else{
+//         temp=temp.next;
+//         return true;
+//         }
+//     }
+// }
+
+class Solution {
+    public ListNode middle(ListNode head){
+        if(head==null || head.next==null)return head;
+         ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
     }
-    ListNode temp;
-    public boolean solution(ListNode right){
-         if(right==null){
-            return true;
+    public boolean isPalindrome(ListNode head) {
+         if(head==null || head.next==null)return true;
+       ListNode slow=middle(head);
+       ListNode newHead=slow.next;
+        slow.next=null;
+        ListNode temp=reverse(newHead);
+        boolean result=true;
+        while(temp!=null){
+            if(temp.val!=head.val){
+                result=false;
+                break;
+            }
+            temp=temp.next;
+            head=head.next;
         }
-        boolean check=solution(right.next);
-        if(check==false)return false;
-        else if(temp.val!=right.val){
-       return false;
+        return result;
+    }
+    public ListNode reverse(ListNode head){
+         if(head==null || head.next==null )return head;
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode currNext=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=currNext;
         }
-        else{
-        temp=temp.next;
-        return true;
-        }
+        return prev;
     }
 }
